b41cdf7cbd381f9e8e7b1bcb4e6421880cb1e596
Joseph Kim <josek16@uci.edu>
2022-11-21 19:31:20 -0800

    receive data from service

--- sleeptracker/src/app/data-view/data-view.page.ts
+++ sleeptracker/src/app/data-view/data-view.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SleepService } from '../services/sleep.service';
+import { SleepData } from '../data/sleep-data';
+import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
+import { OvernightSleepData } from '../data/overnight-sleep-data';
 
 @Component({
   selector: 'app-data-view',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-view.page.scss'],
 })
 export class DataViewPage implements OnInit {
+  AllSleepData:SleepData[];
+	AllOvernightData:OvernightSleepData[];
+	AllSleepinessData:StanfordSleepinessData[];
 
-  constructor() { }
+  constructor(private sleepService: SleepService) { }
 
   ngOnInit() {
+    this.AllSleepData = this.sleepService.getSleepData();
+    this.AllOvernightData = this.sleepService.getOvernightData();
+    this.AllSleepinessData = this.sleepService.getSleepinessData();
   }
 
 }
